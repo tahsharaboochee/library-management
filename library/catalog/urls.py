d7c6d98257c6480662ccc347bc2a02d9cf9c3e16
Tahshara Boochee <boocheet@gmail.com>
2024-04-30 18:59:05 -0700

    Update urls for checkout and return

--- library/catalog/urls.py
+++ library/catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import register, home, book_list, book_detail
+from .views import register, home, book_list, book_detail, checkout_book, return_book
 
 urlpatterns = [
     path('home/', home, name='home'),
@@ -9,4 +9,6 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('books/', book_list, name='book_list'),
     path('books/<int:pk>/', book_detail, name='book_detail'),
+    path('books/<str:id>/checkout/', checkout_book, name='checkout_book'),
+    path('books/<str:id>/return/', return_book, name='return_book'),
 ]
