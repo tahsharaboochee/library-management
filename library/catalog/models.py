c9dfa35b912fe600bb33f439a8dcab2051c488e3
Tahshara Boochee <boocheet@gmail.com>
2024-04-30 18:52:08 -0700

    Circulation model to store circulation data for books

--- library/catalog/models.py
+++ library/catalog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Circulation(models.Model):
+    book_id = models.CharField(max_length=100)  # To store Google Books ID
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    checkout_date = models.DateField(null=True)
+    return_date = models.DateField(null=True)
+    reserved = models.BooleanField(default=False)
