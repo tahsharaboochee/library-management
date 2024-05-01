246dcc9debc7a09fcc0bb519465aa21a1a98a8e0
Tahshara Boochee <boocheet@gmail.com>
2024-04-30 19:00:47 -0700

    Update admin with circulation

--- library/catalog/admin.py
+++ library/catalog/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Book
+from .models import Circulation
 
+@admin.register(Circulation)
+class CirculationAdmin(admin.ModelAdmin):
+    list_display = ('book_id', 'user', 'checkout_date', 'return_date', 'reserved')
+    list_filter = ('user', 'reserved')
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'isbn', 'publication_year')
