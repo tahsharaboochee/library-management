bbfb297c7445593e85e4f279b0430ceb4f746337
Tahshara Boochee <boocheet@gmail.com>
2024-05-03 12:11:22 -0700

    Fixing navbar and display issues

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,1 +1,7 @@
 console.log("Library Management System loaded.");
+document.addEventListener("DOMContentLoaded", function() {
+    var navbarHeight = document.querySelector('.navbar').offsetHeight;
+    console.log(navbarHeight)
+    document.body.style.paddingTop = navbarHeight + 'px';
+});
+
